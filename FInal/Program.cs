4058fada3195b780c71a609aa132ab28485f8eb9
unknown <artem.antonovski@gmail.com>
2022-11-26 18:07:33 +0300

    Final HomeWork 1

--- FInal/Program.cs
+++ FInal/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿string[,] CreateArray()
+{
+    Console.WriteLine("Строки: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Столбцы: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Элемент массива: ");
+    string user = String.Empty;
+
+    string[,] array = new string[rows,columns];
+
+    for (int i = 0;i < rows;i++)
+    {
+        for(int j = 0;j < columns;j++)
+        {
+            Console.WriteLine("Элемент: ");
+            user = Console.ReadLine();
+            array[i,j] = user;
+        }
+    }
+    return array;
+}
+
